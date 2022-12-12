ee2dfc4d8db2d47876a0b6507fd9f6cf9beaa612
vitucelestino <victorcelestino13@gmail.com>
2022-12-12 07:04:28 -0300

    realizando entrega

---
+++ script.js
@@ -0,0 +1,50 @@
+// EXERCICIO 1 - REALIZADO
+
+// let numero1 = prompt('Digite o número 1 aqui');
+// let numero2 = prompt('Digite o número 2 aqui');
+
+// let valor1 = Math.max(numero1, numero2);
+// let valor2 = numero1 - numero2;
+
+// alert(`O maior número é ${valor1} e a diferença é ${valor2}`);
+
+// EXERCICIO 2 - REALIZADO
+
+// let numero1 = parseFloat(prompt('Digite o número 1 aqui'));
+// let numero2 = parseFloat(prompt('Digite o número 2 aqui'));
+
+// let valor1 = Math.max(numero1, numero2);
+// let valor2 = Math.min(numero1, numero2);
+
+// if (numero1 === numero2){
+//     alert(`Números iguais`);
+// } else {
+//     alert(`O número ${valor1} é maior que número ${valor2}`);
+// }
+
+// EXERCICIO 3 - NÃO TO ACERTANDO FAZER
+
+// let salario = parseFloat(prompt('Digite o seu salário'));
+// let prestacao = parseFloat(prompt('Digite o valor da prestação'));
+// let valorpercentual = valorbase * (30 / 100)
+// alert(`${valorpercentual}`)
+// if (valor1 >= valor2) {
+//     alert(`Empréstimo não concedido`);
+// } else {
+//     alert(`Empréstimo concedido`);
+// }
+
+// EXERCIO 4
+
+let numero1 = prompt('Digite o número aqui')
+
+let valor3 = 3;
+let valor5 = 5;
+
+if ((numero1 % valor3) == 0)
+{alert(`Valor divisível por 3`)}
+if ((numero1 % valor5) ==0)
+{alert(`Valor divisível por 5`)}
+// if ((numero1 % valor3, valor5) ==0)
+// {alert(`Valor inválido`)}
+// 
